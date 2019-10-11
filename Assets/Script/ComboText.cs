bb1a7f81ec7cdd98f8443195ed7f6a69445675f9
hina3ta <madomagi1013@gmail.com>
2019-10-11 13:32:24 +0900

    べーたばん

---
+++ Assets/Script/ComboText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script
+{
+    public class ComboText : MonoBehaviour
+    {
+        private Text _comboText;
+
+        void Awake()
+        {
+            _comboText = GetComponent<Text>();
+            _comboText.enabled = false;
+        }
+
+        public void Draw(int combo) {
+            if (combo <= 0) {
+                _comboText.enabled = false;
+                return;
+            }
+
+            if (combo < 5)
+                return;
+
+            _comboText.text = combo.ToString() + " Combo";
+            _comboText.enabled = true;
+        }
+    }
+}
